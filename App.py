bbbb38ce8ab0076c40ac5a596b7f90ffcf7c0352
fahad-mehfooz <fahad@menudata.ai>
2025-03-15 01:51:45 -0400

    Update App.py

--- App.py
+++ App.py
@@ -32,7 +32,7 @@ es_cloud_id = st.secrets["es_cloud_id"]
 openai.api_key = open_api_key
 
 
-es = Elasticsearch(cloud_id=es_cloud_id, basic_auth=(es_username, es_password))
+es = Elasticsearch(cloud_id=es_cloud_id, basic_auth=(es_username, es_password), verify_certs=True, timeout=30)
 
 
 Mocktail_index_name = "mocktail_index"
